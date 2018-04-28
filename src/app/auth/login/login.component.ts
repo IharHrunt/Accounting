c49ecfc9569a5aabc8bd08630981d5905d563537
Ihar Hrunt <Ihar_Hrunt@epam.com>
2018-04-28 17:19:57 +0300

    Added auth, login, registration components, auth, users services

---
+++ src/app/auth/login/login.component.ts
@@ -0,0 +1,69 @@
+import { Component, OnInit } from '@angular/core';
+import { FormGroup, FormControl, Validators} from '@angular/forms';
+import { Router, ActivatedRoute, Params } from '@angular/router';
+import { User } from '../../shared/models/user.model';
+import { Message } from '../../shared/models/message.model';
+import { UsersService } from '../../shared/services/users.service';
+import { AuthService } from '../../shared/services/auth.service';
+
+@Component({
+  selector: 'app-login',
+  templateUrl: './login.component.html',
+  styleUrls: ['./login.component.css']
+})
+export class LoginComponent implements OnInit {
+
+  form: FormGroup;
+  message: Message;
+
+  constructor(
+    private usersService: UsersService,
+    private authService: AuthService,
+    private router: Router,
+    private route: ActivatedRoute
+  ) {}
+
+  ngOnInit() {
+    this.message = new Message('danger', '');
+    this.route.queryParams
+      .subscribe((params: Params) => {
+        if (params['canLogin']) {
+          this.showMessage({
+            text: 'Now you can sign in',
+            type: 'success'
+          });
+        }
+      });
+
+    this.form = new FormGroup({
+      'email': new FormControl(null, [Validators.required, Validators.email]),
+      'password': new FormControl(null, [Validators.required, Validators.minLength(6)])
+    });
+  }
+
+  private showMessage(message: Message) {
+    this.message = message;
+    window.setTimeout(() => {
+      this.message.text = '';
+    }, 5000);
+  }
+
+  onSubmit() {
+    const formData = this.form.value;
+    this.usersService.getUserByEmail(formData.email)
+      .subscribe((user: User) => {
+        if (user) {
+          if (user.password === formData.password) {
+            // logic
+          } else {
+            // console.log('Incorrect password');
+            this.showMessage({text: 'Incorrect password', type: 'danger'});
+          }
+        } else {
+          // console.log('Icorrent user name');
+          this.showMessage({text: 'Icorrent user name', type: 'danger'});
+        }
+      });
+  }
+
+}
